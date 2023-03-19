4193f11ac640e558c187b4a0be64978c03a5749e
mrunalshri <mrunalyetekar@gmail.com>
2023-03-19 01:26:36 +0000

    feat: Added base route components

---
+++ client/src/components/Confession.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Confession: React.FC = () => <>Confession!</>;
+
+export default Confession;
---
+++ client/src/components/Misdemeanour.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Misdemeanour: React.FC = () => <>Misdemeanour!</>;
+
+export default Misdemeanour;
