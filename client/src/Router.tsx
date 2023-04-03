573ef5495f6dae31c825149fcc241198d572fd38
mrunalshri <mrunalyetekar@gmail.com>
2023-04-03 21:54:34 +0100

    feat: added routing for home page

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate, useRoutes } from "react-router-dom";
 import Confession from "./components/Confession";
+import Home from "./components/Home";
 import MainLayout from "./components/MainLayout";
 import Misdemeanours from "./components/Misdemeanour";
 import NotFound from "./components/NotFound";
@@ -11,6 +12,7 @@ const Router: React.FC = () => {
     element: <MainLayout />,
     children: [
       { path: "*", element: <Navigate to="404" /> },
+      { path: "/", element: <Home /> },
       { path: "misdemenour", element: <Misdemeanours /> },
       { path: "confession", element: <Confession /> },
       { path: "404", element: <NotFound /> },
